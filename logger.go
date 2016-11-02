235cf6bb2ddd794f8c6cbc105b3d40dffbe0ecf2
kyf <kyf456@163.com>
2016-11-02 19:02:29 +0800

    debug

--- logger.go
+++ logger.go
@@ -1,29 +1,32 @@
 package martini
 
 import (
-	"log"
+	"github.com/kyf/util/log"
 	"net/http"
-	"time"
 )
 
 // Logger returns a middleware handler that logs the request as it goes in and the response as it goes out.
 func Logger() Handler {
-	return func(res http.ResponseWriter, req *http.Request, c Context, log *log.Logger) {
-		start := time.Now()
+	return func(res http.ResponseWriter, req *http.Request, c Context, logger *log.Logger) {
+		method, path, ip, userAgent := req.Method, req.URL.Path, getRealIP(req), req.UserAgent()
 
-		addr := req.Header.Get("X-Real-IP")
-		if addr == "" {
-			addr = req.Header.Get("X-Forwarded-For")
-			if addr == "" {
-				addr = req.RemoteAddr
-			}
-		}
-
-		log.Printf("Started %s %s for %s", req.Method, req.URL.Path, addr)
-
-		rw := res.(ResponseWriter)
 		c.Next()
 
-		log.Printf("Completed %v %s in %v\n", rw.Status(), http.StatusText(rw.Status()), time.Since(start))
+		rw := res.(ResponseWriter)
+		code := rw.Status()
+
+		logger.Printf("[%s]%s  %s  %d  %s", method, ip, path, code, userAgent)
 	}
+}
+
+func getRealIP(req *http.Request) string {
+	addr := req.Header.Get("X-Real-IP")
+	if addr == "" {
+		addr = req.Header.Get("X-Forwarded-For")
+		if addr == "" {
+			addr = req.RemoteAddr
+		}
+	}
+
+	return addr
 }
